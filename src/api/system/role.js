07be43ae672394b3a7a1daa64727db18cac9b84c
hexu <hexu@mail.taiji.com.cn>
2020-11-15 15:40:31 +0800

    feat:hx-系统管理部分接口对接

--- src/api/system/role.js
+++ src/api/system/role.js
@@ -3,7 +3,7 @@ import request from '@/utils/request'
 // 查询角色列表
 export function listRole(query) {
   return request({
-    url: '/system/role/list',
+    url: '/sysrole/listPage',
     method: 'get',
     params: query
   })
