61a0f49b3d7487d5d2b99e3347f62b24fa310d9a
01384766 <xiaoranguo@sfmail.sf-express.com>
2020-11-24 20:03:47 +0800

    数据字典组件

---
+++ src/commom/data.js
@@ -0,0 +1,9 @@
+
+const data={
+  systemMenuType:[
+    { code: 'MENU', name: '菜单' },
+    { code: 'BUTTON', name: '按钮' },
+    { code: 'PAGE', name: '页面' },
+  ],
+}
+export  default data
---
+++ src/components/select/dictionaryName.vue
@@ -0,0 +1,31 @@
+<template>
+  <span>{{name}}</span>
+</template>
+
+<script>
+import common from 'src/commom/common'
+export default {
+  name: "dictionaryName",
+  props: {
+    value: { //数据字典父选项选项的编号
+      required: true
+    },
+    optionName: { //数据字典父选项选项的编号
+      type: String,
+      required: true
+    }
+  },
+  computed:{
+    name: function () {
+      if(this.value||this.value=='0') {
+        return common.dictionary.getName(this.optionName, this.value);
+      }
+      return null;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
