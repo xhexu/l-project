17777b1b52d8ddcdbcda78f5d478765e7f3a4f9b
hexu <hexu@mail.taiji.com.cn>
2020-12-01 08:46:24 +0800

    fix:报错处理

--- src/main.js
+++ src/main.js
@@ -61,6 +61,7 @@ Vue.component('uploadImg', uploadImg)
 
 import Preview from '@/components/imgShow/imgShow.js';
 Vue.use(Preview);
+Vue.use(permission)
 
 /**
  * If you don't want to use mock-server
